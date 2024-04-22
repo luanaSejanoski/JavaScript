fd8c80e2bbd654c647edf192544521a20b434af8
luanaSejanoski <131304589+luanaSejanoski@users.noreply.github.com>
2024-04-22 17:06:39 -0300

    criei novos exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 30;
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60){ // condição simples
+console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando o sinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Canadá'
+console.log(`Vivendo em ${país}`)
+if(país == 'Itália'){
+console.log('Italiano')
+}
+else{
+    console.log('Estrangeiro')
+}
