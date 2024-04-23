341804d549dd15912e50ad430393f0fe74477403
luanaSejanoski <131304589+luanaSejanoski@users.noreply.github.com>
2024-04-23 15:53:35 -0300

    criei aula12

---
+++ aula12/aula013.js
@@ -0,0 +1,16 @@
+var agora = new Date();
+var hora = agora.getHours();
+  console.log(`Agora são exatamente ${hora} horas`)
+
+ if ( hora < 5){
+    console.log('Boa matina!')
+  }
+  else if( hora < 12){
+    console.log('Bom dia!')
+  }
+  else if (hora <= 18){
+    console.log ('Boa tarde!')
+  }
+  else{
+    console.log('boa noite!')
+  }
---
+++ aula12/ex012.js
@@ -0,0 +1,12 @@
+var idade = 68
+console.log(`Você tem ${idade} anos`)
+
+if( idade < 16 ){
+    console.log('Você não vota')
+}
+else if( idade < 18 || idade > 65){
+        console.log('Seu voto é opcional')
+ }
+else if( idade >= 18){
+    console.log('Seu voto é obrigatório')
+}
